60b30d6c5c6ddf38a3b2a5bc8f76fd3db24f0b0d
desfariaditian <defa.aditian@gmail.com>
2018-04-01 23:42:09 +0700

    modul 6

---
+++ app/src/main/java/com/easy/android/DESFARIADITIAN_1202150081_MODUL6/model/User.java
@@ -0,0 +1,29 @@
+package com.easy.android.DESFARIADITIAN_1202150081_MODUL6.model;
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
